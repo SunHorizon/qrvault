4970c86baf1d34fca6532784bd3a4abcd94c87d7
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2025-07-08 15:19:43 -0400

    configured storage and database

---
+++ src/lib/uploadToSupabase.ts
@@ -0,0 +1,22 @@
+import { supabase } from './supabaseClient'
+
+
+export async function uploadFile(file: File, userId: string){
+    const filePath = `${userId}/${Date.now()}-${file.name}`
+    
+    const { data, error } = await supabase.storage.from('qrcodes').upload(filePath, file);
+
+    if(error) throw new Error(error.message);
+
+    const { data: publicUrlData } = supabase.storage.from('qrcodes').getPublicUrl(filePath);
+
+    await supabase.from('files').insert({
+        
+    })
+    
+    return {
+        filePath,
+        publicUrl: publicUrlData?.publicUrl
+    }
+
+}
